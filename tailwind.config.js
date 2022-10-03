3b489029b4966acc91c7b21a58f8592feedc34e0
hirwaaldo1 <hirwaaldo1@gmail.com>
2022-10-03 14:09:29 +0300

    whole page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'bg-rgba': 'rgba(255, 255, 255, 0.59)',
+        'bg-rgba-2':'rgba(215, 152, 225, 0.15)',
+        'bg-rgba-3':'rgba(97, 153, 237, 0.26)',
+        'bg-rgba-4':"rgba(255, 255, 255, 0.69)"
+      },
+    },
+  },
+  plugins: [],
+}
