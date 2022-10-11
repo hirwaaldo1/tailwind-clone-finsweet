dc3e2e10bf95c7b70f48d9d79a4d1e5128e119a5
hirwaaldo1 <hirwaaldo1@gmail.com>
2022-10-11 13:13:10 +0200

    changing some issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,13 +27,16 @@ module.exports = {
       },
       height: {
         "logo-h":"1.933rem",
-        "448":"28rem"
+        "448":"28rem",
+        '31':"31rem",
+        '80':"80vh"
       },
       width:{
        "logo-w":"8.672rem" ,
        '6.6':"4.153rem",
        '9.10':"90%",
        '170':"10.625rem",
+       '90':"32.75rem",
       },
       lineHeight:{
         "normal":"110%",
@@ -55,6 +58,13 @@ module.exports = {
       },
       borderRadius: {
         "md":"4.188rem"
+      },
+      blur: {
+        ex:"6.25rem",
+        xs: '12.5rem',
+      },
+      zIndex: {
+        'n/1': '-1000',
       }
     },
   },
